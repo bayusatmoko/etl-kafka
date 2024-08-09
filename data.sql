98449fbf88b666ca9c7627f8fd88f228c4e5a24b
bayusatmoko <52567519+bayusatmoko@users.noreply.github.com>
2024-08-09 18:02:25 +0000

    Finish

---
+++ data.sql
@@ -0,0 +1,23 @@
+select user, host, plugin from mysql.user;
+DROP USER 'root'@'%';
+
+CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'maulana';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+
+CREATE USER 'maulana'@'%' IDENTIFIED WITH mysql_native_password BY 'maulana';
+GRANT ALL PRIVILEGES ON *.* TO 'maulana'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+
+--it is executed from SQL-Yog
+CREATE TABLE maulana.my_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    NAME VARCHAR(50),
+    age INT
+);
+
+INSERT INTO maulana.my_table (NAME, age) VALUES ('John', 30), ('Alice', 25), ('Bob', 35), ('Emily', 28);
+
+SELECT * FROM maulana.my_table;
